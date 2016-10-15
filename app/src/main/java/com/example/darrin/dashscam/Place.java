7320bf6ba82b3676bcb42ce8dd376a72cdc6523a
Darrin Fong <darrinfong@gmail.com>
2016-10-15 01:59:29 -0400

    plan b: Eat where? IDK...

---
+++ app/src/main/java/com/example/darrin/dashscam/Place.java
@@ -0,0 +1,18 @@
+package com.example.darrin.dashscam;
+
+/**
+ * Created by Darrin on 2016-10-15.
+ */
+
+class Place {
+    String NAME;
+    String DESCRIPTION;
+    String ADDRESS;
+
+    Place(String name, String description, String address){
+        NAME = name;
+        DESCRIPTION = description;
+        ADDRESS = address;
+    }
+}
+
